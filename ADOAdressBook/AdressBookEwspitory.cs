80b86d8e0a45b3e392d99f3e0778e991e8a8b8df
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-24 16:15:19 +0530

    Ability to retrieve entries sorted alphabetically

--- ADOAdressBook/AdressBookEwspitory.cs
+++ ADOAdressBook/AdressBookEwspitory.cs
@@ -137,6 +137,25 @@ namespace ADO_AddressBook.ADO_AddressBook
             }
             return nameList;
         }
+        //UC 7: Ability to retrieve entries sorted alphabetically
+        public string PrintSortDataBasedOnCity(string City)
+        {
+            string nameList = "";
+            //query to be executed
+            string query = "select * from Address_Book_Table where City='" + City + "' order by(FirstName)";
+            SqlCommand sqlCommand = new SqlCommand(query, this.sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+                while (sqlDataReader.Read())
+                {
+                    DisplayEmployeeDetails(sqlDataReader);
+                    nameList += sqlDataReader["FirstName"].ToString() + " ";
+                }
+            }
+            return nameList;
+        }
        
 
         public void DisplayEmployeeDetails(SqlDataReader sqlDataReader)
