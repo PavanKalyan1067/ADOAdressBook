f62f39272f0a9458080779f11e74010e2887628b
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-24 16:00:22 +0530

    Welcome to the ADO AdressBook

---
+++ ADOAdressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOAdressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO AdressBook");
+        }
+    }
+}
